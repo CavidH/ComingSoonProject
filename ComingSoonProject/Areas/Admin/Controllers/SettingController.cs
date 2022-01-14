6056f99e13dc8da3e3f269095f2a02c9936d4a0f
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-14 18:23:52 +0400

    :persevere::persevere::persevere:

---
+++ ComingSoonProject/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using ComingSoonProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComingSoonProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SettingController : Controller
+    {
+        private AppDbContext _context { get;}
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            var settings = await _context.Settings.ToListAsync();
+            return View(settings);
+        }
+    }
+}
