9ce6d2fb421b4c971f55ec60748d6c20c1f9f2ad
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-14 16:44:57 +0400

    Js fix

--- ComingSoonProject/wwwroot/Assets/Js/script.js
+++ ComingSoonProject/wwwroot/Assets/Js/script.js
@@ -1,72 +1,34 @@
-(function($) {
-    "use strict";
-    // var end = $('.popup').attr("data-target");
-    $.fn.aksCountDown = function(options) {
-        const aks = $(this);
-        var settings = $.extend({
-                endTime: "",
-                refresh: 1000,
-                onEnd: function() {}
-            },
-            options
-        );
-        return this.each(function(i) {
-            function endTimeCheck(d1, d2) {
-                return (
-                    d1.getFullYear() === d2.getFullYear() &&
-                    d1.getMonth() === d2.getMonth() &&
-                    d1.getDate() === d2.getDate()
-                );
-            }
+let comingDate = new Date('Feb 29, 2022 ')
 
-            function countTimer() {
-                var endTime = new Date(settings.endTime);
-                endTime = Date.parse(endTime) / 1000;
+let d = document.getElementById('days')
+let h = document.getElementById('hours')
+let m = document.getElementById('minutes')
+let s = document.getElementById('seconds')
 
-                var now = new Date();
-                now = Date.parse(now) / 1000;
+let x = setInterval(function () {
+    let now = new Date()
+    let selisih = comingDate.getTime() - now.getTime()
 
-                var timeLeft = endTime - now;
+    let days = Math.floor(selisih / (1000 * 60 * 60 * 24))
+    let hours = Math.floor(selisih % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    let minutes = Math.floor(selisih % (1000 * 60 * 60) / (1000 * 60))
+    let seconds = Math.floor(selisih % (1000 * 60) / 1000)
 
-                var days = Math.floor(timeLeft / 86400);
-                var hours = Math.floor((timeLeft - days * 86400) / 3600);
-                var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
-                var seconds = Math.floor(
-                    timeLeft - days * 86400 - hours * 3600 - minutes * 60
-                );
+    d.innerHTML = getTrueNumber(days)
+    h.innerHTML = getTrueNumber(hours)
+    m.innerHTML = getTrueNumber(minutes)
+    s.innerHTML = getTrueNumber(seconds)
 
-                if (hours < "10") {
-                    hours = "0" + hours;
-                }
-                if (minutes < "10") {
-                    minutes = "0" + minutes;
-                }
-                if (seconds < "10") {
-                    seconds = "0" + seconds;
-                }
-
-                $(aks).find("[data-days]").html(days);
-                $(aks).find("[data-hours]").html(hours);
-                $(aks).find("[data-minutes]").html(minutes);
-                $(aks).find("[data-seconds]").html(seconds);
-            }
-            var now = new Date();
-            var endTime = new Date(settings.endTime);
-
-            if (endTimeCheck(now, endTime) === true) {
-                settings.onEnd.call(aks);
-            } else {
-                setInterval(function() {
-                    countTimer();
-                }, settings.refresh);
-            }
-        });
-    };
-})(jQuery);
-
-$("#timer").aksCountDown({
-    endTime: "15 June 2022 9:56:00 GMT+01:00",
-    onEnd: function() {
-        $(this).html('<div class="timer-end">Finished Time</div>');
+    if (selisih < 0) {
+        clearInterval(x)
+        d.innerHTML = '00'
+        h.innerHTML = '00'
+        m.innerHTML = '00'
+        s.innerHTML = '00'
     }
-});
+}, 1000)
+
+function getTrueNumber(x) {
+    if (x < 10) return '0' + x
+    else return x
+}
