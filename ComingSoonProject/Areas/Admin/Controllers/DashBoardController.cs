0bcc6f9c66de507bb85170e3201c9b2e53f8b2db
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-14 17:42:32 +0400

    Admin panel start

---
+++ ComingSoonProject/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComingSoonProject.Areas.Admin.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ComingSoonProject/Startup.cs
+++ ComingSoonProject/Startup.cs
@@ -49,7 +49,7 @@ namespace ComingSoonProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+                    pattern: "{area:exists}/{controller=DashBoard}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
