533e138b273b20f1dd9bcd8da4c927549f3c7880
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-16 14:07:42 +0400

    Register VM done

---
+++ ComingSoonProject/VievModels/UserRegisterVM.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ComingSoonProject.VievModels
+{
+    public class UserRegisterVM
+    {
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
